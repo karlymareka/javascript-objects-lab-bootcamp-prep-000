3e232208486e31fbb2dfc30eeacc41b53a81b002
karly.mareka@gmail.com <karly.mareka@gmail.com>
2018-05-26 18:08:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var recipes = new Object();
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({},object)
+  return Object.assign({},object)
   return object
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  var newObj = Object.assign({}, object)
+  
   
 }
