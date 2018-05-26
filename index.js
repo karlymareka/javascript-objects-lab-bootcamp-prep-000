aee69a69e333757876e51ac33c0db242e3f875c5
karly.mareka@gmail.com <karly.mareka@gmail.com>
2018-05-26 18:01:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = new Object();
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object = {key: value}
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object = {key: value}
+  return object 
+}
